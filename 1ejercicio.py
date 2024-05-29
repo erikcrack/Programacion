38cd7215e2f60654958b8db6a4d4452b577f5f8a
erikcrack <128084414+erikcrack@users.noreply.github.com>
2024-05-29 15:11:36 -0600

    Add files via upload

---
+++ 1ejercicio.py
@@ -0,0 +1,6 @@
+'''Este programa esta elaborado por arreglos alamacenando nombre, apellido materno y paterno'''
+print('Hola Que tal')
+Nombres = input('Introduce tu nombre:  ')
+Apellido_Paterno = input('Escribe tu nombre Paterno:  ')
+Apellido_Materno = input('Escribe Apellido Materno;')
+print('Hola' + Nombres,Apellido_Paterno )
